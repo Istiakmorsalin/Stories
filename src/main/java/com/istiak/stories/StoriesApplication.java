2b7393a461b864649879ab2be8314ccecc6d7c54
Istiak Morsalin <anik.cse@gmail.com>
2018-09-20 01:01:56 +0600

    [Task: #Phase 1] Subject: Create new Notes [Description]:  - Initial Commit Signed-off-by: Istiak Morsalin <anik.cse@gmail.com>

---
+++ src/main/java/com/istiak/stories/StoriesApplication.java
@@ -0,0 +1,14 @@
+package com.istiak.stories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class StoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoriesApplication.class, args);
+	}
+}
