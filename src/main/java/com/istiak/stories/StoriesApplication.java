663ea209e23ff3d7f7622bfb22287e83ec602109
Istiak Morsalin <anik.cse@gmail.com>
2018-09-21 01:46:49 +0600

    [Task: #Phase 5] Subject: authentication [Description]:  - generated an instance of BCryptPasswordEncode Signed-off-by: Istiak Morsalin <anik.cse@gmail.com>

--- src/main/java/com/istiak/stories/StoriesApplication.java
+++ src/main/java/com/istiak/stories/StoriesApplication.java
@@ -4,9 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// ... other imports
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class StoriesApplication {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoriesApplication.class, args);
