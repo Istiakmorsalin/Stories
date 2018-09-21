6baac7cd1c6d6c0f0c97a5084fe51be6cac0fa06
Istiak Morsalin <anik.cse@gmail.com>
2018-09-21 19:56:16 +0600

    [Task: #Phase 6] Subject: web app [Description]  - http encoder basic service added Signed-off-by: Istiak Morsalin <anik.cse@gmail.com>

---
+++ Story_Client/src/app/core/service/http/http-service.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { environment } from './../../../../environments/environment';
+
+@Injectable()
+export class HttpRequestService {
+
+  constructor(
+    private http: Http,
+  ) { }
+
+  createAuthorizationHeader(headers: Headers) {
+
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept-Language', environment.lang);
+    
+  }
+
+  get(url, params: any = '') {
+    const headers = new Headers();
+    this.createAuthorizationHeader(headers);
+    return this.http.get(url, {
+      search: params,
+      headers: headers
+    });
+  }
+
+  post(url, data) {
+    const headers = new Headers();
+    this.createAuthorizationHeader(headers);
+
+    return this.http.post(url, data, {
+      headers: headers
+    });
+  }
+
+  put(url, data) {
+    const headers = new Headers();
+    this.createAuthorizationHeader(headers);
+    return this.http.put(url, data, {
+      headers: headers
+    });
+  }
+
+  delete(url, data?) {
+    const headers = new Headers();
+    this.createAuthorizationHeader(headers);
+
+    if (data) {
+      return this.http.delete(url, new RequestOptions({
+        headers: headers,
+        body: data
+      }));
+    } else {
+      return this.http.delete(url, {
+        headers: headers
+      });
+    }
+
+  }
+
+  patch(url, data) {
+    const headers = new Headers();
+    this.createAuthorizationHeader(headers);
+    return this.http.patch(url, data, {
+      headers: headers
+    });
+  }
+
+
+
+}
