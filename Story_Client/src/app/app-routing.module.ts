810f985553388153e6933899eaf487adbfe153cd
Istiak Morsalin <anik.cse@gmail.com>
2018-09-23 15:00:15 +0600

    [Task: #Phase 6] Subject: web app [Description]  - edit and list added to routing module Signed-off-by: Istiak Morsalin <anik.cse@gmail.com>

--- Story_Client/src/app/app-routing.module.ts
+++ Story_Client/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
 import {LoginComponent}   from './login/login.component';
 import {CreateComponent}   from './story/create/create.component';
+import {ListComponent}   from './story/list/list.component';
+import {EditComponent}   from './story/edit/edit.component';
 
 export const appRoutes: Routes = [{
   path: 'registration',
@@ -12,7 +14,13 @@ export const appRoutes: Routes = [{
 }, {
   path: 'create-story',
   component: CreateComponent
-}, 
+}, {
+  path: 'story-list',
+  component: ListComponent
+},{
+  path: 'edit-story/:id',
+  component: EditComponent
+},
 
 
 ];
